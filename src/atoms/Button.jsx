4283429e68a1461c7cd8ce6f766115ea0702d784
remi <remimillet@gmail.com>
2019-03-28 20:48:30 +0100

    Add hover handling in button

--- src/atoms/Button.jsx
+++ src/atoms/Button.jsx
@@ -1,20 +1,42 @@
 import styled, { css } from "styled-components";
+import { darken } from 'polished';
+
 import {getColors} from "../utils/styling";
 
 
 const cssForPrimary = ({ theme, primary }) => {
-  if(!primary){
+  const backgroundColor = getColors(theme).green;
+  const hoverBackgroundColor = darken(0.05, backgroundColor);
+
+  return css`
+    background-color: ${backgroundColor};
+    color: ${getColors(theme).white};
+    &:hover {
+      background-color: ${hoverBackgroundColor};
+    }
+  `
+}
+
+const cssForSecondary = ({ theme, primary, secondary }) => {
+  //If primary is set to true it will override secondary prop
+  if(primary || !secondary){
     return;
   }
+  const backgroundColor = getColors(theme).yellow;
+  const hoverBackgroundColor = darken(0.05, backgroundColor);
   return css`
-    background-color: ${getColors(theme).green};
-    color: ${getColors(theme).white};
+    background-color: ${backgroundColor};
+    color: ${getColors(theme).black};
+    &:hover {
+      background-color: ${hoverBackgroundColor};
+    }
   `
 }
 
 
 const Button = styled("button")`
   ${props => cssForPrimary(props)}
+  ${props => cssForSecondary(props)}
 `
 
 

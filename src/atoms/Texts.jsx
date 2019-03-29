d9f5c17b5363cc7e03c80e1337e42a26a864a5ea
remi <remimillet@gmail.com>
2019-03-29 10:57:33 +0100

    Implements texts

---
+++ src/atoms/Texts.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+
+const H1 = styled("h2")`
+  font-size: 48px;
+`
+
+const H2 = styled("h3")`
+  font-size: 40px;
+`
+
+const H3 = styled("h4")`
+  font-size: 32px;
+`
+
+const H4 = styled("h5")`
+  font-size: 24px;
+`
+
+const H5 = styled("h6")`
+  font-size: 16px;
+`
+
+const H6 = styled("h1")`
+  font-size: 14px;
+`
+
+const TextBig = styled("p")`
+  font-size: 18px;
+`
+
+const Text = styled("p")`
+  font-size: 16px;
+`
+
+const TextSmall = styled("p")`
+  font-size: 14px;
+`
+
+const TextXSmall= styled("p")`
+  font-size: 18px;
+`
+
+export default { H1, H2, H3, H4, H5, H6, TextBig, Text, TextSmall, TextXSmall };

c992c103efaad65f8d03705a87a80f4cf173d441
remi <remimillet@gmail.com>
2019-04-02 11:16:20 +0200

    Add Grid system to the project

--- src/utils/styling/defaultTheme.js
+++ src/utils/styling/defaultTheme.js
@@ -1,4 +1,41 @@
-
+const customConf = {
+  mediaQuery: 'only screen',
+  columns: {
+    xs: 4,
+    sm: 8,
+    md: 8,
+    lg: 12,
+    xl: 12,
+  },
+  gutterWidth: {
+    xs: 1,
+    sm: 1,
+    md: 1.5,
+    lg: 1.5,
+    xl: 1.5,
+  },
+  paddingWidth: {
+    xs: 1,
+    sm: 1,
+    md: 1.5,
+    lg: 1.5,
+    xl: 1.5,
+  },
+  container: {
+    xs: 'full', // 'full' = max-width: 100%
+    sm: 'full', // 'full' = max-width: 100%
+    md: 'full', // 'full' = max-width: 100%
+    lg: 90, // max-width: 1440px
+    xl: 90, // max-width: 1440px
+  },
+  breakpoints: {
+    xs: 1,
+    sm: 48, // 768px
+    md: 64, // 1024px
+    lg: 90, // 1440px
+    xl: 120, // 1920px
+  },
+}
 
 const theme = {
   colors: {
@@ -13,7 +50,8 @@ const theme = {
     grey4: "#606060",
     red: "#EB1212",
     white: "#FFFFFF",
-  }
+  },
+  awesomegrid: customConf
 };
 
 export default theme;

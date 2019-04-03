0d1de3eb198efedc4dfd994ee0e97c0b140632ae
remi <remimillet@gmail.com>
2019-04-03 15:21:29 +0200

    Fix depracated API

--- src/atoms/Logo/Logo.jsx
+++ src/atoms/Logo/Logo.jsx
@@ -2,11 +2,13 @@ import LogoImg from './MiurosLogo.png'
 import LogoImgDark from './MiurosLogoDark.png'
 import styled from "styled-components";
 
-const Logo = styled("img").attrs({
-    src: ({ dark }) => dark ? LogoImgDark : LogoImg
+const Logo = styled('img').attrs(({dark}) => {
+    return {
+        src: dark ? LogoImgDark : LogoImg
+    }
 })`
-    width: 116px;
-    height: 40px;
+width: 116px;
+height: 40px;
 `
 
 export default Logo

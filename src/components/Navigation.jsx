4e6e05243a8e3b9d87b6ae0524dfb47403b71326
Maxime Salomon <maxime@croissant.io>
2019-04-05 13:12:48 +0200

    modified Assist dropdown content

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -29,7 +29,7 @@ const dropdownItems = [
       <ListItem.DropdownItem
         title="Assist"
         link="/assist"
-        description="Assist your agents increase their efficiency and first reply times"
+        description="Assist increase your agents efficiency & lower first reply times"
         color="#FFC509"
         icon={`url('${assistLinkImage}')`}
       />

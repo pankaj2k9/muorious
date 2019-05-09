338c7b6c8edca67b163d3eefea5689b8523a980f
Alexander Gasnikov <alexander.gasnikov@gmail.com>
2019-05-09 13:28:12 +0200

    Add GA and Hubspot tracking code

--- src/html.js
+++ src/html.js
@@ -27,6 +27,19 @@ export default class HTML extends React.Component {
         />
 
         {this.props.headComponents}
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-77360862-1', 'auto');
+          ga('set', 'anonymizeIp', true);
+          ga('send', 'pageview');
+        </script>
+
+        <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/2795220.js"></script>
+
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=UA-77360862-1"
